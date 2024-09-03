80a33747b27d750697aa0c1ada4c6ad84da6c66e
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2024-09-03 19:49:35 -0300

    refactor: removed showcase junk files

--- src/app/showcase/showcase.module.ts
+++ src/app/showcase/showcase.module.ts
@@ -4,11 +4,10 @@ import { NgModule } from '@angular/core';
 import { ButtonShowcaseModule } from './button-showcase/button-showcase.module';
 import { IconShowcaseModule } from './icon-showcase/icon-showcase.module';
 import { ShowcaseRoutingModule } from './showcase-routing.module';
-import { ShowcaseComponent } from './showcase.component';
 
 @NgModule({
-  declarations: [ShowcaseComponent],
+  declarations: [],
   imports: [ButtonShowcaseModule, CommonModule, IconShowcaseModule, ShowcaseRoutingModule],
-  exports: [ShowcaseComponent],
+  exports: [],
 })
 export class ShowcaseModule {}
