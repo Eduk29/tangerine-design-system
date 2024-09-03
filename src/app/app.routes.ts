b24ffe3c4c56662cd3028a807a36038b9720efa4
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2024-09-03 19:50:08 -0300

    chore: impl. lazy load routes to app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const appBaseRoutes: Routes = [
+  { path: 'showcase', loadChildren: () => import('./showcase/showcase.module').then(m => m.ShowcaseModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
