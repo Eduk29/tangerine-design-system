a98def3c010bb9161462cb5bce8ec1f3a6d7403d
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2024-10-28 18:26:57 -0300

    refactor: generated new build

---
+++ dist/tangerine-design-system/browser/chunk-7JLXNOX4.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-3WSBSE2W.js";import"./chunk-HNYWVO5M.js";import"./chunk-AAR65G36.js";import"./chunk-27UWYZHQ.js";export{a as ShowcaseModule};
---
+++ dist/tangerine-design-system/browser/chunk-PLVTVSXI.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-JXN4NA6O.js";import"./chunk-AAR65G36.js";import"./chunk-27UWYZHQ.js";export{a as HomeModule};
