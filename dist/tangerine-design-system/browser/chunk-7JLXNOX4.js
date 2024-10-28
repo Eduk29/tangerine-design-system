9e96052817cf954d59c9319f939274e14dd2bb50
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2024-10-28 20:12:30 -0300

    refactor: generate new build with formfield adjusts

--- dist/tangerine-design-system/browser/chunk-7JLXNOX4.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-3WSBSE2W.js";import"./chunk-HNYWVO5M.js";import"./chunk-AAR65G36.js";import"./chunk-27UWYZHQ.js";export{a as ShowcaseModule};
--- dist/tangerine-design-system/browser/chunk-PLVTVSXI.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-JXN4NA6O.js";import"./chunk-AAR65G36.js";import"./chunk-27UWYZHQ.js";export{a as HomeModule};
